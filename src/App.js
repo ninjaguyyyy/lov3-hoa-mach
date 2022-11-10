0bc96943ff65348884f44bb0d39235f190e96d8b
Chi Nguyen <chinh@dipro.vn>
2022-11-10 20:07:43 +0700

    build: implement for my love

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Typical from "react-typical";
+import UnLock from "./open-padlock.png";
+import Lock from "./lock.png";
+import { useState } from "react";
+import { Button, Input, Modal, ModalBody, ModalFooter } from "reactstrap";
+import Heart from "./red-heart_2764-fe0f.gif";
 
 function App() {
+  const [isLock, setIsLock] = useState(true);
+  const [isOpenModal, setIsOpenModal] = useState(false);
+
+  const toggleModal = () => setIsOpenModal(!isOpenModal);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div class="my_girl">
+        <p>Như Hòa</p>
+      </div>
+      <div className="lock" onClick={toggleModal}>
+        <img src={isLock ? Lock : UnLock} alt="lock" />
+      </div>
+      <Typical steps={["Hello", 1000, "Hello world!", 500]} loop={Infinity} wrapper="p" />
+
+      <Modal isOpen={isOpenModal} toggle={toggleModal}>
+        <ModalBody style={{ background: "#ea80b0" }}>
+          <Input name="password" placeholder="Enter a password" size={"lg"} />
+          <div className="modal_submit">
+            <div className="question">
+              Hòa <img src={Heart} alt="heart" width={40} height={40} /> Chí ?
+            </div>
+            <Button color="warning" size="lg" onClick={toggleModal}>
+              Có
+            </Button>
+          </div>
+        </ModalBody>
+      </Modal>
     </div>
   );
 }
