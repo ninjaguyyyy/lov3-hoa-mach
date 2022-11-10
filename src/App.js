c4e1b63a5725a6e4e731f578b896cabab3138896
Chi Nguyen <chinh@dipro.vn>
2022-11-10 20:23:58 +0700

    feat: implement enter password

--- src/App.js
+++ src/App.js
@@ -9,8 +9,25 @@ import Heart from "./red-heart_2764-fe0f.gif";
 function App() {
   const [isLock, setIsLock] = useState(true);
   const [isOpenModal, setIsOpenModal] = useState(false);
+  const [wrongPasswordMsg, setWrongPasswordMsg] = useState("");
+  const [password, setPassword] = useState("");
 
-  const toggleModal = () => setIsOpenModal(!isOpenModal);
+  const toggleModal = () => {
+    setWrongPasswordMsg("");
+    setIsOpenModal(!isOpenModal);
+  };
+
+  const handleSubmitPassword = () => {
+    setWrongPasswordMsg("Hmm.. Sai ròi!");
+  };
+
+  const handleChangePassword = (e) => {
+    const value = e.target.value;
+    setPassword(value);
+    if (value === "") {
+      setWrongPasswordMsg("");
+    }
+  };
 
   return (
     <div>
@@ -24,14 +41,17 @@ function App() {
 
       <Modal isOpen={isOpenModal} toggle={toggleModal}>
         <ModalBody style={{ background: "#ea80b0" }}>
-          <Input name="password" placeholder="Enter a password" size={"lg"} />
+          <Input value={password} name="password" placeholder="Enter a password" size={"lg"} onChange={handleChangePassword} />
           <div className="modal_submit">
-            <div className="question">
-              Hòa <img src={Heart} alt="heart" width={40} height={40} /> Chí ?
+            <div className="err">{wrongPasswordMsg}</div>
+            <div className="buttons">
+              <div className="question">
+                Hòa <img src={Heart} alt="heart" width={40} height={40} /> Chí ?
+              </div>
+              <Button color="warning" size="lg" onClick={handleSubmitPassword}>
+                Có
+              </Button>
             </div>
-            <Button color="warning" size="lg" onClick={toggleModal}>
-              Có
-            </Button>
           </div>
         </ModalBody>
       </Modal>
